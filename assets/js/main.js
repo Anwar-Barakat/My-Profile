571db40e24819253f9450e13e28bc7b3a6f3beef
Anwar Barakat <brkatanwar0@gmail.com>
2022-09-12 21:21:24 +0300

    finishing portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,13 +36,56 @@ const tabsContainer = document.querySelector(".about-tabs"),
     aboutSection = document.querySelector(".about-section");
 
 tabsContainer.addEventListener("click", (e) => {
-    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+    if (
+        e.target.classList.contains("tab-item") &&
+        !e.target.classList.contains("active")
+    ) {
         tabsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add('active');
+        e.target.classList.add("active");
 
-        const target = e.target.getAttribute('data-target');
-        aboutSection.querySelector('.tab-content.active').classList.remove('active');
+        const target = e.target.getAttribute("data-target");
+        aboutSection
+            .querySelector(".tab-content.active")
+            .classList.remove("active");
 
-        aboutSection.querySelector(target).classList.add('active')
+        aboutSection.querySelector(target).classList.add("active");
     }
+});
+
+// ! ===========================
+// !Portfolio Popup
+// ! ===========================
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("view-peoject")) {
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0, 0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp-thumbnail img").src =
+        portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML =
+        portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body .description").innerHTML =
+        portfolioItem.querySelector(".description").innerHTML;
+
+    document.querySelector(".pp-body .general-info").innerHTML =
+        portfolioItem.querySelector(".general-info").innerHTML;
+}
+
+document.querySelector(".pp-close").onclick = togglePortfolioPopup;
+
+// hide popup when clicking out of it:
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("pp-inner")) togglePortfolioPopup();
 });
