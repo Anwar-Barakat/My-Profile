5ceef48e31bde0e7c065e55fcee0ff9f902b77f7
Anwar Barakat <brkatanwar0@gmail.com>
2022-09-12 14:21:52 +0300

    finishing about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,48 @@
-//? =========================== Change Website Color  ====================
+// ! ===========================
+// !Change Website Color
+// ! ===========================
 let toggleBtn = document.querySelector(".nav__color-icon"),
-  colorContainer = document.querySelector(".nav__color");
+    colorContainer = document.querySelector(".nav__color");
 
 toggleBtn.addEventListener("click", () => {
-  colorContainer.classList.toggle("active");
+    colorContainer.classList.toggle("active");
 });
 
 var colors = document.getElementsByClassName("change-color");
 for (i = 0; i < colors.length; i++) {
-  colors[i].addEventListener("click", changeColor);
+    colors[i].addEventListener("click", changeColor);
 }
 
 const selectedColor = localStorage.getItem("selected-color");
 
 if (selectedColor) {
-  document.documentElement.style.setProperty("--hue-color", selectedColor);
+    document.documentElement.style.setProperty("--hue-color", selectedColor);
 }
 
 var style = getComputedStyle(document.body).getPropertyValue("--hue-color");
 
 function changeColor() {
-  var color = this.getAttribute("data-color");
-  var style = getComputedStyle(document.body).getPropertyValue("--hue-color");
+    var color = this.getAttribute("data-color");
+    var style = getComputedStyle(document.body).getPropertyValue("--hue-color");
 
-  localStorage.setItem("selected-color", style);
-  document.documentElement.style.setProperty("--hue-color", color);
+    localStorage.setItem("selected-color", style);
+    document.documentElement.style.setProperty("--hue-color", color);
 }
-//? =========================== Change Website Color  ====================
+
+// ! ===========================
+// !Change About Tab
+// ! ===========================
+const tabsContainer = document.querySelector(".about-tabs"),
+    aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-item") && !e.target.classList.contains("active")) {
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add('active');
+
+        const target = e.target.getAttribute('data-target');
+        aboutSection.querySelector('.tab-content.active').classList.remove('active');
+
+        aboutSection.querySelector(target).classList.add('active')
+    }
+});
