c5b0027a6d949f473434f8a7f9497cd23d3b2bd4
Anwar Barakat <brkatanwar0@gmail.com>
2022-09-26 22:26:45 +0300

    minor changes on about us page

--- assets/js/dark-light-mode.js
+++ assets/js/dark-light-mode.js
@@ -1,7 +1,7 @@
 //? =========================== Dark/Light Mode  =========================
 const themeButton = document.getElementById("theme-btn");
 const darkTheme = "dark-theme";
-const iconTheme = "fas-sun";
+const iconTheme = "uil-sun";
 
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
@@ -9,22 +9,34 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
     document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "fas-moon" : "fas-sun";
+    themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 if (selectedTheme) {
     document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
         darkTheme
     );
-    themeButton.classList[selectedIcon === "fas-moon" ? "add" : "remove"](
+    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
         iconTheme
     );
 }
 
 themeButton.addEventListener("click", () => {
-    alert("hi");
     document.body.classList.toggle(darkTheme);
     themeButton.classList.toggle(iconTheme);
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
+
+    let logoImage = document.querySelector(".logo");
+    logoImage.classList.toggle("light");
+    if (logoImage.classList.contains("light")) {
+        document.querySelector(".mode-text").innerHTML = "Dark Mode";
+
+        logoImage.innerHTML =
+            '<img src="https://i.postimg.cc/QCyRWB1P/IMG-20220826-172957-743-removebg-preview.png" alt="IMG-20220826-172957-743-removebg-preview"/>';
+    } else {
+        document.querySelector(".mode-text").innerHTML = "Light Mode";
+        logoImage.innerHTML =
+            '<img src="https://i.postimg.cc/Dz6pXJKg/IMG-20220826-173001-148-removebg-preview.png" alt="IMG-20220826-173001-148-removebg-preview"/>';
+    }
 });
 //? =========================== Dark/Light Mode  =========================
