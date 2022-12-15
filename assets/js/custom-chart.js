5f834112585e3f15e8900d876953a7159b708d56
Anwar Barakat <brkatanwar0@gmail.com>
2022-12-16 00:56:00 +0200

    add a real info about myself, in addition to some minor enhancment part(3)

--- assets/js/custom-chart.js
+++ assets/js/custom-chart.js
@@ -1,58 +1,56 @@
 const frontend = document.getElementById("frontend").getContext("2d");
 const myFrontend = new Chart(frontend, {
-    type: "bar",
-    data: {
-        labels: ["HTML", "CSS", "JavaScript", "Bootstrap", "Tailwind", "VueJS"],
-        datasets: [
-            {
-                label: "Frontend",
-                data: [60, 75, 70, 90, 80, 65],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgb(75, 192, 192)",
-                    "rgb(54, 162, 235)",
-                    "rgb(153, 102, 255)",
-                ],
-                borderWidth: 1,
-            },
+  type: "bar",
+  data: {
+    labels: ["HTML", "CSS", "JavaScript", "Bootstrap", "Tailwind"],
+    datasets: [
+      {
+        label: "Frontend",
+        data: [60, 75, 70, 90, 80],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
         ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-            },
-        },
-    },
+  },
 });
 
 const backend = document.getElementById("backend").getContext("2d");
 const myBackend = new Chart(backend, {
-    type: "polarArea",
-    data: {
-        labels: ["PHP", "MySQL", "Laravel", "Livewire", "AlpineJS"],
-        datasets: [
-            {
-                label: "backend",
-                data: [60, 80, 70, 90, 80],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                ],
-            },
+  type: "polarArea",
+  data: {
+    labels: ["PHP", "MySQL", "Laravel", "Livewire", "AlpineJS"],
+    datasets: [
+      {
+        label: "backend",
+        data: [60, 80, 70, 90, 80],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
         ],
-    },
+      },
+    ],
+  },
 });
