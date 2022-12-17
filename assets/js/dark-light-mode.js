d2a30bc73d77a6f17efd255f150097a518b6133e
Anwar Barakat <brkatanwar0@gmail.com>
2022-12-17 16:47:46 +0200

    add a real info about myself, in addition to some minor enhancment part(4)

--- assets/js/dark-light-mode.js
+++ assets/js/dark-light-mode.js
@@ -1,29 +1,36 @@
 //? =========================== Dark/Light Mode  =========================
-const themeButton = document.getElementById("theme-btn");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const themeButton = document.getElementById("theme-btn"),
+  darkTheme = "dark-theme",
+  iconTheme = "uil-sun",
+  selectedTheme = localStorage.getItem("selected-theme"),
+  selectedIcon = localStorage.getItem("selected-icon");
 
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const logo = document.querySelector(".logo img");
 
 const getCurrentTheme = () =>
-    document.body.classList.contains(darkTheme) ? "dark" : "light";
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 if (selectedTheme) {
-    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-        darkTheme
-    );
-    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-        iconTheme
-    );
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
+document.body.classList.contains("dark-theme")
+  ? logo.classList.add("dark")
+  : logo.classList.remove("dark");
 
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
-    localStorage.setItem("selected-theme", getCurrentTheme());
-    localStorage.setItem("selected-icon", getCurrentIcon());
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+  document.body.classList.contains("dark-theme")
+    ? logo.classList.add("dark")
+    : logo.classList.remove("dark");
 });
 //? =========================== Dark/Light Mode  =========================
